85c7c8eb70be318fb172a8358d15a3b8cba22aea
Frank Febbraro <frankfebbraro@gmail.com>
2010-08-05 21:49:51 +0000

    Revamped the UI for bulk processing

--- calais.admin.js
+++ calais.admin.js
@@ -18,3 +18,29 @@ Drupal.behaviors.calais_admin = function(context) {
   });
   
 }
+
+Drupal.behaviors.calais_enqueue = function(context) {
+
+  // Hide the queue form on close
+  $("#calais-queue-form #edit-close", context).click(function() {
+    $("#calais-queue-form").hide(500);
+  });
+  
+  // Display the queue form filled with the data from the proper content type
+  $("a.calais-queue-toggle", context).click(function(){
+    var type = $(this).attr('id').split('calais-queue-toggle-')[1];
+    if ($("#calais-queue-form").is(':visible') && $("#edit-type").val() == type) {
+      $("#calais-queue-form").hide(500)
+    }
+    else {
+      var config = Drupal.settings.calais_queue;
+      $("#edit-type").val(type);
+      $('#calais-queue-type-label').html(type);
+      $("#edit-process-type-" + config[type].process).attr('checked', true);
+      $("#edit-threshold").val(config[type].threshold);
+      $("#calais-queue-form").show(500);
+    }
+    return false;
+  });
+  
+}
